55f0fd54cd61dfd0ef57481380ca5519e1fa8e53
SanketLV <sanketlakhani80@gmail.com>
2024-12-18 22:17:54 +0530

    sidebar and header added

---
+++ constants/index.ts
@@ -0,0 +1,30 @@
+export const navItems = [
+  {
+    name: "Dashboard",
+    icon: "/assets/icons/dashboard.svg",
+    url: "/",
+  },
+  {
+    name: "Documents",
+    icon: "/assets/icons/documents.svg",
+    url: "/documents",
+  },
+  {
+    name: "Images",
+    icon: "/assets/icons/images.svg",
+    url: "/images",
+  },
+  {
+    name: "Media",
+    icon: "/assets/icons/video.svg",
+    url: "/media",
+  },
+  {
+    name: "Others",
+    icon: "/assets/icons/others.svg",
+    url: "/others",
+  },
+];
+
+export const avatarPlaceholderUrl =
+  "https://images.pexels.com/photos/7862484/pexels-photo-7862484.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
-import { createAdminClient } from "@/lib/appwrite";
+import { createAdminClient, createSessionClient } from "@/lib/appwrite";
 import { appwriteConfig } from "@/lib/appwrite/config";
 import { ID, Query } from "node-appwrite";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
@@ -48,7 +49,7 @@ export const createAccount = async ({
 
   if (!accountId) throw new Error("Failed to send an email OTP");
 
-  if (existingUser) {
+  if (!existingUser) {
     const { databases } = await createAdminClient();
 
     await databases.createDocument(
@@ -58,8 +59,7 @@ export const createAccount = async ({
       {
         fullName,
         email,
-        avatar:
-          "https://www.pexels.com/photo/shallow-focus-of-a-boy-holding-a-game-controller-7862484/",
+        avatar: avatarPlaceholderUrl,
         accountId,
       }
     );
@@ -92,4 +92,20 @@ export const verifyEmailOTP = async ({
   } catch (error) {
     handleError(error, "Failed to verify email OTP");
   }
+};
+
+export const getCurrentUser = async () => {
+  const { account, databases } = await createSessionClient();
+
+  const result = await account.get();
+
+  const user = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("accountId", result.$id)]
+  );
+
+  if (user.total <= 0) return null;
+
+  return parseStringify(user.documents[0]);
 };
