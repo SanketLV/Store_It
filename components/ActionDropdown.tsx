86127d285f2bf402e5d761d7757edf3852344598
SanketLV <sanketlakhani80@gmail.com>
2024-12-20 17:47:41 +0530

    share file functionality added

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -23,9 +23,9 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { renameFile } from "@/lib/actions/file.actions";
+import { renameFile, updateFileUsers } from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
-import { FileDetails } from "./ActionsModalContent";
+import { FileDetails, ShareInput } from "./ActionsModalContent";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -33,6 +33,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [action, setAction] = useState<ActionType | null>(null);
   const [name, setName] = useState(file.name);
   const [isLoading, setIsLoading] = useState(false);
+  const [emails, setEmails] = useState<string[]>([]);
 
   const path = usePathname();
 
@@ -41,7 +42,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     setIsDropdownOpen(false);
     setAction(null);
     setName(file.name);
-    // setEmails([]);
+    setEmails([]);
   };
 
   const handleAction = async () => {
@@ -53,7 +54,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     const actions = {
       rename: () =>
         renameFile({ fileId: file.$id, name, extension: file.extension, path }),
-      share: () => console.log("share"),
+      share: () => updateFileUsers({ fileId: file.$id, emails, path }),
       delete: () => console.log("delete"),
     };
 
@@ -62,6 +63,19 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     if (success) closeAllModals();
 
     setIsLoading(false);
+  };
+
+  const handleRemoveUser = async (email: string) => {
+    const updatedEmails = emails.filter((e) => e !== email);
+
+    const success = await updateFileUsers({
+      fileId: file.$id,
+      emails: updatedEmails,
+      path,
+    });
+
+    if (success) setEmails(updatedEmails);
+    closeAllModals();
   };
 
   const redrerDialogContent = () => {
@@ -83,6 +97,13 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
             />
           )}
           {value === "details" && <FileDetails file={file} />}
+          {value === "share" && (
+            <ShareInput
+              file={file}
+              onInputChange={setEmails}
+              onRemove={handleRemoveUser}
+            />
+          )}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
           <DialogFooter className="flex flex-col gap-3 md:flex-row">
--- components/ActionsModalContent.tsx
+++ components/ActionsModalContent.tsx
@@ -3,11 +3,20 @@ import React from "react";
 import Thumbnail from "./Thumbnail";
 import FormattedDateTime from "./FormattedDateTime";
 import { convertFileSize, formatDateTime } from "@/lib/utils";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+interface Props {
+  file: Models.Document;
+  onInputChange: React.Dispatch<React.SetStateAction<string[]>>;
+  onRemove: (email: string) => void;
+}
 
 const ImageThumbnail = ({ file }: { file: Models.Document }) => (
   <div className="file-details-thumbnail">
     <Thumbnail type={file.type} extension={file.extension} url={file.url} />
-    <div className="flex flex-col">
+    <div className="flex flex-col text-left">
       <p className="subtitle-2 mb-1">{file.name}</p>
       <FormattedDateTime date={file.$createdAt} className="caption" />
     </div>
@@ -30,6 +39,58 @@ export const FileDetails = ({ file }: { file: Models.Document }) => {
         <DetailRow label="Size:" value={convertFileSize(file.size)} />
         <DetailRow label="Owner:" value={file.owner.fullName} />
         <DetailRow label="Last edit:" value={formatDateTime(file.$updatedAt)} />
+      </div>
+    </>
+  );
+};
+
+export const ShareInput = ({ file, onInputChange, onRemove }: Props) => {
+  return (
+    <>
+      <ImageThumbnail file={file} />
+
+      <div className="share-wrapper">
+        <p className="subtitle-2 pl-1 text-light-100">
+          Share file with other users
+        </p>
+        <Input
+          type="email"
+          placeholder="Enter email address"
+          onChange={(e) => onInputChange(e.target.value.trim().split(","))}
+          className="share-input-field"
+        />
+
+        <div className="pt-4">
+          <div className="flex justify-between">
+            <p className="subtitle-2 text-light-100">Shared with</p>
+            <p className="subtitle-2 text-light-200">
+              {file.users.length} users
+            </p>
+          </div>
+
+          <ul className="pt-2">
+            {file.users.map((email: string) => (
+              <li
+                key={email}
+                className="flex items-center justify-between gap-2"
+              >
+                <p className="subtitle-2">{email}</p>
+                <Button
+                  onClick={() => onRemove(email)}
+                  className="share-remove-user"
+                >
+                  <Image
+                    src="/assets/icons/remove.svg"
+                    alt="Remove"
+                    width={24}
+                    height={24}
+                    className="remove-icon"
+                  />
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
